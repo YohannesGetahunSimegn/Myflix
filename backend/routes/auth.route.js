e8f79620a4f25378815bc8ffedb78f09215b72c1
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-08-18 17:42:11 +0300

    Backend basic setup and database connected

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,17 +1,12 @@
 import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/api/v1/signup", (req, res) => {
-  res.send("Signup route");
-});
+router.get("/signup", signup);
 
-router.get("/api/v1/login", (req, res) => {
-  res.send("Login route");
-});
+router.get("/login", login);
 
-router.get("/api/v1/logout", (req, res) => {
-  res.send("Logout route");
-});
+router.get("/logout", logout);
 
 export default router;
