b02069ce51fac79bdd544fd6878550ccf48d5ef8
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-08-20 17:59:07 +0300

    Search, Search history and delete history implemented

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 import { protectRoute } from "./middleware/protectRoute.js";
 
 import { ENV_VARS } from "./config/envVars.js";
@@ -18,6 +19,7 @@ app.use(cookieParser()); // allows to parse cookies from req
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:" + PORT);
