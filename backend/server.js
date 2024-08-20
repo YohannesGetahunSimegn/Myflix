058b030b5004063af918f6e954437ca0b5bfc44c
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-08-20 15:09:42 +0300

    Routes protected/ authorization done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
@@ -11,10 +13,11 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // allow as to parse req.body
+app.use(cookieParser()); // allows to parse cookies from req
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:" + PORT);
