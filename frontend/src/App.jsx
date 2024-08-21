cdd2c8a8e93e9b2edc433593883678de902853ef
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-08-21 11:51:34 +0300

    Front end setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import SignupPage from "./Pages/SignupPage";
+import HomePage from "./Pages/HomePage";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  );
+}
