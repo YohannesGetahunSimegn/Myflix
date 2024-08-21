e44ab6ce1480d3279248406b6e8ac60a9310af1a
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-08-21 19:10:27 +0300

    AuthScreen Page completed and Footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
 import HomePage from "./Pages/home/HomePage";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
--- frontend/src/Pages/home/AuthScreen.jsx
+++ frontend/src/Pages/home/AuthScreen.jsx
@@ -48,7 +48,6 @@ const AuthScreen = () => {
       <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
 
       {/* 1st Section */}
-
       <div className="py-10 bg-black text-white">
         <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
           {/* Left side */}
@@ -62,8 +61,123 @@ const AuthScreen = () => {
             </p>
           </div>
           {/* Right side */}
+          <div className="flex-1 relative">
+            <img src="/tv.png" alt="Tv image" className="mt-4 z-20 relative" />
+            <video
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-1/2 z-10"
+              playsInline
+              autoPlay={true}
+              muted
+              loop
+            >
+              <source src="/hero-vid.m4v" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
+      {/* Separator */}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      {/* 2nd Section */}
+      <div className="py:10 bg-black text-white">
+        <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2">
+          {/* Left Side */}
           <div className="flex-1">
-            <img src="/tv.png" alt="Tv image" className="mt-4" />
+            <div className="relative">
+              <img
+                src="/stranger-things-lg.png"
+                alt="Stranger Things image"
+                className="mt-4"
+              />
+              <div className="flex items-center gap-2 absolute bottom-5 left-1/2 -translate-x-1/2 bg-black w-3/4 lg:w-1/2 h-24 border border-slate-500 rounded-md px-2 ">
+                <img
+                  src="/stranger-things-sm.png"
+                  alt="image"
+                  className="h-full"
+                />
+                <div className="flex justify-between items-center w-full">
+                  <div className="flex flex-col gap-0">
+                    <span className="text-md lg:text-lg font-bold">
+                      Stranger Things
+                    </span>
+                    <span className="text-sm text-blue-500">
+                      Downloading...
+                    </span>
+                  </div>
+                  <img src="/download-icon.gif" alt="" className="h-12" />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Right Side */}
+          <div className="flex-1 md:text-left text-center">
+            <h1 className="text-4xl md:text-6xl font-bold mb-4">
+              Unlimited movies, Tv shows, and more
+            </h1>
+            <p className="text-lg mb-4">Watch anywhere. Cancel anytime.</p>
+            <p className="mb-4">
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/*Separator */}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      {/* 3rd Section */}
+      <div className="py-10 bg-black text-white">
+        <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+          {/* Left side */}
+          <div className="flex-1 text-center md:text-left">
+            <h2 className="text-4xl md:text-5x1 font-extrabold mb-4">
+              Watch everywhere
+            </h2>
+            <p className="text-lg md:text-xl">
+              Stream unlimited movies and TV shows on your phone, tablet, laptop
+              and Tv.
+            </p>
+          </div>
+          {/* Right side */}
+          <div className="flex-1 relative overflow-hidden">
+            <img
+              src="/device-pile.png"
+              alt="Device image"
+              className="mt-4 z-20 relative"
+            />
+            <video
+              className="absolute top-2 left-1/2 -translate-x-1/2 h-4/6 z-10 max-w-[63%]"
+              playsInline
+              autoPlay={true}
+              muted
+              loop
+            >
+              <source src="/video-devices.m4v" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
+
+      {/*Separator */}
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      {/* 4th Section */}
+      <div className="py-10 bg-black text-white">
+        <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2">
+          {/* Left side */}
+          <div className="flex-1 relative">
+            <img src="/kids.png" alt="Enjoy your Tv" className="mt-4" />
+          </div>
+          {/* Right side */}
+          <div className="flex-1 text-center md:text-left">
+            <h2 className="text-4xl md:text-5x1 font-extrabold mb-4">
+              Create profile for kids
+            </h2>
+            <p className="text-lg md:text-xl">
+              Send kids on adventures with their favorite characters in a space
+              made for them-free with your membership.
+            </p>
           </div>
         </div>
       </div>
